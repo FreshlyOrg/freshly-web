f1615657bc7c77cec453bc88d2d16d64bedd51c8
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-11 18:14:17 -0700

    Moved initial response to be at /api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var port = process.env.port || 8080;
 var app = express();
 
-app.get('/', function(req, res, next) {
+app.get('/api', function(req, res, next) {
   res.send('hi');
 });
 
