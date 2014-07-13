10fb0d06d64c7396030a9ca9fc7dbff2251639eb
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-12 19:04:58 -0700

    Changed server to allow cross origin requests

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.use(bodyParser.json());
 // ===================
 app.use('/', express.static(__dirname + '/public'));
 
+// ALLOW CROSS ORIGIN REQUESTS
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 
 // ROUTES FOR API
 // ==============
