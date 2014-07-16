f219a56071ed997239371a01115129e8188501dd
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-16 09:59:02 -0700

    Updated activity schema to include cost and time Updated activity routes to update cost and time on PUT and POST requests

--- server/activities/activityRoutes.js
+++ server/activities/activityRoutes.js
@@ -33,6 +33,8 @@ router.route('/activities/:activity_id')
       //Update activity
       activity.name = req.body.name;
       activity.description = req.body.description;
+      activity.cost = req.body.cost;
+      activity.time = req.body.time;
       activity.lat = req.body.lat;
       activity.lng = req.body.lng;
       activity.tags = req.body.tags;
@@ -97,6 +99,8 @@ router.route('/activities')
     var activity = new Activity();
     activity.name = req.body.name;
     activity.description = req.body.description;
+    activity.cost = req.body.cost;
+    activity.time = req.body.time;
     activity.lat = req.body.lat;
     activity.lng = req.body.lng;
     activity.tags = req.body.tags;
