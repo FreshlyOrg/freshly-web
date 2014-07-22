047e4410820d0f19117034d53f1364d3512dba4d
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-21 20:09:19 -0700

    Added comments to each file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Get references to code in other files
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,14 +8,19 @@ var Activity = require('./server/activities/activityModel');
 // BASIC SETUP
 // ===========
 
+//Defines mongo connection for azure deploy (or, failing that, for local deploy)
 var mongooseConnectionURL = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/freshly'
 mongoose.connect(mongooseConnectionURL)
 
-//Defines port for azure deploy (and local deploy)
+//Defines port for azure deploy (or, failing that, for local deploy)
 var port = process.env.port || 8080;
 
 //Parses incoming form data onto request.body
+
+//Not strictly necessary, but nice to have for testing with Google's Postman tool
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//The mobile app will primarily be sending data in JSON format
 app.use(bodyParser.json());
 
 // STATIC FILE SERVING
@@ -34,17 +40,23 @@ app.all('*', function(req, res, next) {
 // ==============
 var router = express.Router();
 
-//test route
+//Base route--not strictly necessary
 router.get('/', function(req, res) {
   res.json({ message: 'Welcome to the freshly API!' });
 });
 
+//Defines the activity routes--mongoose is passed in so images can be added/accessed
+//using gridfs-stream (more info in activityRoutes.js)
 var ActivityRoutes = require('./server/activities/activityRoutes.js');
 var activityRouter = ActivityRoutes(mongoose);
 
 // REGISTER ROUTES
 // ===============
+
+//Only used with the base route so far, so not strictly necessary at the moment
 app.use('/api', router);
+
+//All addresses starting with /api/activities will be handled by activityRouter
 app.use('/api/activities', activityRouter);
 
 
--- server/activities/activityModel.js
+++ server/activities/activityModel.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//Tells mongoose the expected format for "activity"
+//objects...this determines how they get saved in the Mongo DB
 var ActivitySchema = new Schema({
   name: String,
   description: String,
@@ -20,7 +22,8 @@ var ActivitySchema = new Schema({
   updated_at: { type: Date }
 });
 
-//Adds timestamps
+//Adds/updates timestamps to activity objects before they
+//are added to the database
 ActivitySchema.pre('save', function(next) {
   var now = new Date();
   this.updated_at = now
@@ -29,7 +32,6 @@ ActivitySchema.pre('save', function(next) {
   }
   next();
 })
-
 
 module.exports = mongoose.model('FreshlyActivity', ActivitySchema);
 
